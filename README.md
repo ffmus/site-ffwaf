e0b396a2bb8fa2f75fdc341bd95c63e7d1f2ec94
Paulo da Silva <paulo.da.silva@vitroconnect.de>
2014-11-28 22:42:00 +0100

    0.5.1-exp20141116

---
+++ README.md
@@ -0,0 +1,6 @@
+
+site.conf and site.mk to build gluon for Freifunk Warendorf.
+
+Build are currently made available at http://images.freifunk-muensterland.net/
+
+
---
+++ site.mk
@@ -0,0 +1,26 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged
+
+
+DEFAULT_GLUON_RELEASE := 0.5.1
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0

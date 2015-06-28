9c857515c3ad0b473f1a7722d434fa51369b76e2
Paulo da Silva <paulo.da.silva@vitroconnect.de>
2015-06-28 15:16:42 +0200

    Merge beta into stable

--- site.mk
+++ site.mk
@@ -40,7 +40,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 2015.1.1-1.2beta$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2015.1.1-2
 
 
 ##	GLUON_RELEASE
